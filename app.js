096039daa377117bbfcdee17015ba85af07ac7f5
akabab <ycribier@student.42.fr>
2014-05-17 18:32:47 +0200

    stable

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 var express = require('express');
 var socket = require('socket.io');
 var http = require('http');
+var path = require('path');
 
 var app = express();
 var server = http.createServer(app);
-//var io = socket.listen(server);
 
 server.listen(8080);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use(app.router);
+
+//ROUTES
 app.get('/', function (req, res) {
 	res.render('index');
 });
 
-// io.sockets.on('connection', function (client) {
-// 	console.log("Client connected..");
 
-// 	client.emit('message', { message: 'Hello world' });
-// 	client.on('my other event', function (data) {
-// 	console.log(data);
-//   });
-// });
+//SOCKET CONNECTION
+var io = socket.listen(server);
+
+io.sockets.on('connection', function (client) {
+	console.log("Client connected..");
+
+	client.emit('message', { message: 'Hello world' });
+	client.on('answer', function (data) {
+		console.log(data.my);
+	});
+});
